f2f3753d1a0f40b6c0594d7cca91091bc4b976d7
verpiamet-tejclv255 <83222147+verpiamet-tejclv255@users.noreply.github.com>
2021-04-29 19:01:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php
-// Query params are:  
-
-require_once dirname(__FILE__) . '/kclient.php';
-$client = new KClient('https://asojerko.xyz/api.php?', '2Ydk5YdVGy9rLtJG');
-$client->sendAllParams();       // to send all params from page query
-$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
-// $client->param('sub_id_5', '123'); // you can send any params
-// $client->keyword('PASTE_KEYWORD');  // send custom keyword
-// $client->currentPageAsReferrer(); // to send current page URL as click referrer
-// $client->disableSessions(); // to disable using session cookie (without this cookie restoreFromSession wouldn't work)
-// $client->debug();              // to enable debug mode and show the errors
-// $client->execute();             // request to api, show the output and continue
-$client->executeAndBreak();     // to stop page execution if there is redirect or some output
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
